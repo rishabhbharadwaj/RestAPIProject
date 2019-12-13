ec709ceaa8444cbd630b2642554c8ba64ad58f41
rishabhbharadwaj <rishabhbharadwaj1996@gmail.com>
2019-12-14 00:30:08 +0530

    This is for testing purpose

--- RestAPI/src/main/java/com/qa/client/RestClient.java
+++ RestAPI/src/main/java/com/qa/client/RestClient.java
@@ -16,6 +16,7 @@ public class RestClient {
 
 	//1. GET Method
 	public void get(String url) throws ClientProtocolException, IOException {
+		
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpGet httpGet = new HttpGet(url); //HTTP get request
 		CloseableHttpResponse closeableHttpResponse = httpClient.execute(httpGet); // hit the GET URL
@@ -37,11 +38,7 @@ public class RestClient {
 		}
 		System.out.println("Headers Array ---> "+ allHeaders);
 		
-		
 	}
-	
-	
-	
 	
 	
 	
