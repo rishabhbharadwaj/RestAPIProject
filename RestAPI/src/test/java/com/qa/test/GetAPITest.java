42f6750f34897023ad6c6c73070f5c0d14703d1e
rishabhbharadwaj <rishabhbharadwaj1996@gmail.com>
2019-12-15 16:19:40 +0530

    Some changes have been done in RestClient and GetAPITest classes. New Class TestUtill is created,

--- RestAPI/src/test/java/com/qa/test/GetAPITest.java
+++ RestAPI/src/test/java/com/qa/test/GetAPITest.java
@@ -1,13 +1,18 @@
 package com.qa.test;
 
 import java.io.IOException;
-
+import java.util.HashMap;
+import org.apache.http.Header;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.qa.base.TestBase;
 import com.qa.client.RestClient;
+import com.qa.util.TestUtil;
 
 public class GetAPITest extends TestBase{
 
@@ -16,6 +21,7 @@ public class GetAPITest extends TestBase{
 	String apiURL;
 	String url;
 	RestClient restClient;
+	CloseableHttpResponse closeableHttpResponse;
 	
 	@BeforeMethod
 	public void setUp() throws ClientProtocolException, IOException {
@@ -28,9 +34,30 @@ public class GetAPITest extends TestBase{
 	@Test
 	public void getAPITest() throws ClientProtocolException, IOException {
 		restClient = new RestClient();
-		restClient.get(url);
+		closeableHttpResponse = restClient.get(url);
+		
+		//a. Status Code
+		int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+		System.out.println("Status Code ---> "+ statusCode);
+		
+		Assert.assertEquals(statusCode,RESPONSE_STATUS_CODE_200,"Status Code is not 200");
+		
+		//b. JSON String
+		String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(),"UTF-8");
+		JSONObject responseJson = new JSONObject(responseString); 
+		System.out.println("Response JSON form API ---> "+ responseJson);
+		
+		String s = TestUtil.getValueByJPath(responseJson,"/per_page");
 		
 		
+		//c. All headers
+		Header[] headerResponse = closeableHttpResponse.getAllHeaders();
+		HashMap<String, String> allHeaders = new HashMap<String, String>();
+		for(Header header: headerResponse) {
+			allHeaders.put(header.getName(), header.getValue());
+		}
+		System.out.println("Headers Array ---> "+ allHeaders);
+	
 	}
 	
 }
